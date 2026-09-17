9d20f240de38bb685bf3306f6fd4005fc4ebb57f
fabriziograciosii <fabriziograciosi.it@gmail.com>
2026-09-17 12:34:24 -0300

    clase ArmaBase

---
+++ Armas/ArmaBase.ts
@@ -0,0 +1,16 @@
+export abstract class ArmaBase {
+    private municion : number; 
+    private danio : number; 
+
+    constructor(municionInicial: number, danioInicial: number) {
+        this.municion = municionInicial
+        this.danio = danioInicial
+    }
+
+    public apretarGatillo() : number {
+
+        const danioCausado = this.municion > 0 ? this.municion -1 : 0;
+
+        return danioCausado;
+    }
+}
